13125e51658dba0f4b14ddbed47db24f044085b0
ArmellaJuan <juanmarcosarmella@gmail.com>
2016-02-29 20:10:12 -0300

    Adding detail page, ui-route, etc.

--- app/app.js
+++ app/app.js
@@ -2,16 +2,30 @@
 
 // Declare app level module which depends on views, and components
 angular.module('movieCatalogApp', [
-  'ngRoute',
+  'ui.router',
   'ngResource',
   'movieCatalogApp.filters',
   'movieCatalogApp.directives',
   'movieCatalogApp.services',
-  'movieCatalogApp.view1',
-  'movieCatalogApp.view2',	
-  'movieCatalogApp.viewMovieTest'
+  'movieCatalogApp.viewAllMovies',
+  'movieCatalogApp.viewDetail'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/views/view1'});
-}]);
 
+config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /all-movies
+  $urlRouterProvider.otherwise("/all-movies");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('all-movies', {
+      url: "/all-movies",
+      templateUrl: "views/all-movies/all-movies.html",
+      controller: 'allMoviesController'
+    })
+    .state('all-movies.detail', {
+      url: "/detail/{idMovie}",
+      templateUrl: "views/detail/detail.html",
+      controller: 'detailController'
+    });
+});
